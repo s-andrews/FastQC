b62c160c4dc12b17b6e23219117bb82f52102398
s-andrews <simon.andrews@babraham.ac.uk>
2018-01-17 14:02:27 +0000

    Bumped version to 0.11.8.devel

--- uk/ac/babraham/FastQC/FastQCApplication.java
+++ uk/ac/babraham/FastQC/FastQCApplication.java
@@ -54,7 +54,7 @@ import uk.ac.babraham.FastQC.Utilities.NanoporeBasename;
 
 public class FastQCApplication extends JFrame {	
 	
-	public static final String VERSION = "0.11.7";
+	public static final String VERSION = "0.11.8.devel";
 	
 	private JTabbedPane fileTabs;
 	private WelcomePanel welcomePanel;
