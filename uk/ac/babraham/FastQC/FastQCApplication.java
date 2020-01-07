b46eabebe7c5e6ce3df139175ac7aca13386afd4
s-andrews <simon.andrews@babraham.ac.uk>
2020-01-07 16:34:48 +0000

    Add support for multi-fast5 files.  Relates to #41

--- uk/ac/babraham/FastQC/FastQCApplication.java
+++ uk/ac/babraham/FastQC/FastQCApplication.java
@@ -54,7 +54,7 @@ import uk.ac.babraham.FastQC.Utilities.NanoporeBasename;
 
 public class FastQCApplication extends JFrame {	
 	
-	public static final String VERSION = "0.11.8";
+	public static final String VERSION = "0.11.9.devel";
 	
 	private JTabbedPane fileTabs;
 	private WelcomePanel welcomePanel;
