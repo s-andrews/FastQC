62bfb33f064f0347a1317f7078268d902c4d8906
Peter Kneale <Peter.Kneale@sydney.edu.au>
2025-09-19 03:04:45 +0000

    Add JUnit testing framework and initial smoke test
    - Update .gitignore to include test directories
    - Modify build.xml to support test compilation and execution
    - Add JUnit and Hamcrest libraries to the lib directory
    - Create a basic smoke test class

---
+++ test/SmokeTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SmokeTest {
+
+    @Test
+    public void smokeTest() {
+        assertTrue(true);
+    }
+}
