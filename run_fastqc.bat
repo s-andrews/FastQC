08a3c280d18100e145e5c9fd480a30a551f7d298
Matthieu Muffato <mm49@sanger.ac.uk>
2026-03-06 21:15:58 +0000

    Added missing JAR file and updated the classpath (incl. in the launchers)

--- run_fastqc.bat
+++ run_fastqc.bat
@@ -1,1 +1,1 @@
-java -Xmx250m -classpath .;./sam-1.103.jar;./jbzip2-0.9.jar uk.ac.babraham.FastQC.FastQCApplication %*
+java -Xmx250m -classpath .;./htsjdk.jar;./jbzip2-0.9.jar;./cisd-jhdf5.jar;./commons-compress-1.26.0.jar uk.ac.babraham.FastQC.FastQCApplication %*
